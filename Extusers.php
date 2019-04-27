3e86af8c88bbe8e4502f56305dfae4c55105252b
siddarthareddy <reddy.siddartha53@gmail.com>
2019-04-26 21:48:12 -0700

    extusers

--- Extusers.php
+++ Extusers.php
@@ -3,14 +3,14 @@
         $ch = curl_init();
 
         // set url
-        curl_setopt($ch, CURLOPT_URL, "http://janakidwadasi.xyz/goGirls/getUsers.php"); 
+        curl_setopt($ch, CURLOPT_URL, "http://janakidwadasi.xyz/goGirls/getUsers.php");
 
         //return the transfer as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         // $output contains the output string
         $output = curl_exec($ch);
-
+        echo $output;
         // close curl resource to free up system resources
         curl_close($ch);
 ?>
