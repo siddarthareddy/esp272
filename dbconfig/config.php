b5bc7a721746147433c708efc9b16ea39c9dc678
siddarthareddy <reddy.siddartha53@gmail.com>
2019-04-26 20:54:42 -0700

    db config changes

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,3 +1,3 @@
 <?php
-  $con = mysqli_connect("localhost", "ubuntu", "", "company") or die("Unable to connect");
+  $con = mysqli_connect("localhost", "ubuntu", "Siddartha4!", "company") or die("Unable to connect");
 ?>
