12c7b5e3880d9694d2c60bcfe4736e5aff17a4fd
siddarthareddy <reddy.siddartha53@gmail.com>
2019-04-26 21:37:47 -0700

    dc

--- Lusers.php
+++ Lusers.php
@@ -1,17 +1,11 @@
 <?php include "framework/head.html"; ?>
 <?php include "framework/nav.php"; ?>
 <?php
-$sql="SELECT fname, lname, email, address, hphone, cphone FROM users";
+$sql="SELECT fname, lname FROM users";
 $result = mysqli_query($con,$sql);
-echo "<table border='1'>";
-echo "<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Address</th><th>Home Phone</th><th>Cell Phone</th></tr>";
-while ($row = $result->fetch_row()) {
-    echo "<tr>";
-    foreach($row as $r){
-        echo "<td>$r</td>";
-    }
-    echo "</tr>";
+while ($row = $result->fetch_assoc()) {
+    echo $row['fname']." ".echo $row['lname'];
+    echo nnl2br("\n");
 }
-echo "</table>";
 $result->close();
 ?>
